81aef48b5f9c87f717b9c4a441ef8bea3ac812bd
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-03-06 18:47:11 -0300

    Player Move and Lerp

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    public Vector2 previousPosition;
+    public float velocity = 1f;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Move(Input.mousePosition.x - previousPosition.x);
+        }
+
+        previousPosition = Input.mousePosition;
+    }
+
+    public void Move(float speed)
+    {
+        transform.position += Vector3.right * (Time.deltaTime * speed * velocity);
+    }
+}
