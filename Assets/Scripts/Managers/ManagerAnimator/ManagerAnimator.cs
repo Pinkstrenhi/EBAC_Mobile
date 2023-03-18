060a9deabb9484fb239f0f6a50f8aab9908dc2c2
Pinkstrenhi <82385580+Pinkstrenhi@users.noreply.github.com>
2023-03-18 17:10:13 -0300

    Manager Animator

---
+++ Assets/Scripts/Managers/ManagerAnimator/ManagerAnimator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerAnimator : MonoBehaviour
+{
+    public Animator animator;
+    public List<SetupAnimator> setupAnimators;
+    public enum AnimationType
+    {
+        Idle, 
+        Run, 
+        Dead
+    }
+
+    public void Play(AnimationType animationType)
+    {
+        setupAnimators.ForEach(i =>
+        {
+            if (i.animationType == animationType)
+            {
+                animator.SetTrigger(i.animationTrigger);
+            } 
+        });
+        /*foreach (var setupAnimator in setupAnimators)
+        {
+            if (setupAnimator.animationType == animationType)
+            {
+                animator.SetTrigger(setupAnimator.animationTrigger);
+                break;
+            }
+        }*/
+    }
+
+    private void Update()
+    {
+        PlayAnimation(KeyCode.Alpha1,AnimationType.Run);
+        PlayAnimation(KeyCode.Alpha2,AnimationType.Dead);
+        PlayAnimation(KeyCode.Alpha3,AnimationType.Idle);
+    }
+
+    private void PlayAnimation(KeyCode keycode, AnimationType animationType)
+    {
+        if (Input.GetKeyDown(keycode))
+        {
+            Play(animationType);
+        } 
+    }
+}
+[System.Serializable]
+public class SetupAnimator
+{
+    public ManagerAnimator.AnimationType animationType;
+    public string animationTrigger;
+    
+}
